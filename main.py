c5e431484fcec359fcbc22a7aa109524388236e7
hecarimday <hecarimday@naver.com>
2024-10-16 20:52:25 +0900

    init: main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    result = []
+    for i in range(len(nums)):
+      for j in range(i+1, len(nums)):
+        if nums[i] + nums[j] == target:
+          result.append(i)
+          result.append(j)
+          return result
+    return []
